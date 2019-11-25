cc415d24501c55982f4d93b66bee2442260d4d70
BitVivAZ <jembuxmann@gmail.com>
2019-11-26 00:32:17 +0200

    added timestamp

--- database/initialize.py
+++ database/initialize.py
@@ -2,8 +2,11 @@ from config import matches_requested
 from get_match import get_latest_matches
 from heroes import get_heroes
 import database as db
+import datetime
+
 
 def initialize():
+    print(datetime.datetime.now())
     print('INITIALIZING')
     db.check_tables_exists()
     get_heroes()
