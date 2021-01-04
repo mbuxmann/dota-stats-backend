ee81983284770eabb1159d88d99c789250357219
Martin Buxmann <Jembuxmann@gmail.com>
2021-01-04 10:35:21 +0200

    Update config.py

--- database/config.py
+++ database/config.py
@@ -8,6 +8,6 @@ debug_heroes = False
 database = '/home/ubuntu/dota-stats-backend/database/database.db'
 #database = 'database.db'
 matches_requested = 30
-WebAPIKey = "7ED28ADAFD5EE5F46D041EF05E65E78F"
+WebAPIKey = WEBAPIKEY
 steam_id = 126747751
 
